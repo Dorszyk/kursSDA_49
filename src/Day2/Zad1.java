f1e51d17c6d387c5e7a615adde678cb9fc451b83
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-09-11 21:43:04 +0200

    Day2Zad1

---
+++ src/Day2/Zad1.java
@@ -0,0 +1,23 @@
+package Day2;
+
+public class Zad1 {
+    public static void main(String[] args) {
+
+        System.out.println("Last char: " + getLastCharacterV1("domek"));
+        System.out.println("Last char: " + getLastCharacterV2("domek"));
+
+    }
+
+    private static char getLastCharacterV1(String input) {
+
+        int lastPosition = input.length() - 1;
+        return input.charAt(lastPosition);
+    }
+
+    private static String getLastCharacterV2(String input) {
+
+        int lastPosition = input.length() - 1;
+        return input.substring(lastPosition);
+    }
+}
+}
