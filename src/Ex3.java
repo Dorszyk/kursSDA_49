09f74d2cd2954e47c30fc10782766d9fed620748
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-09-11 12:34:04 +0200

    zadanie3

--- src/Ex3.java
+++ src/Ex3.java
@@ -5,12 +5,24 @@ public class Ex3 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Proszę wprowadz pierwszą liczbę :" );
+        System.out.print("Proszę wprowadz pierwszą liczbę :");
         int firstDigit = scanner.nextInt();
 
-        System.out.println("Proszę wpisać drugą liczbę " );
+        System.out.print("Proszę wpisać drugą liczbę ");
         int secondDigit = scanner.nextInt();
-
         boolean result;
+
+        result = firstDigit > secondDigit ? true : false;
+        System.out.println("Czy x jest większe od y: " + result);
+
+        result = firstDigit * 3 > secondDigit ? true : false;
+        System.out.println("Czy x*3 jest większe od y: " + result);
+
+        result = (secondDigit++ < ++firstDigit) && (--firstDigit < secondDigit++);
+        System.out.println(result);
+
+        result = (firstDigit * secondDigit) % 2 == 0;
+        System.out.println(result);
+
     }
 }
