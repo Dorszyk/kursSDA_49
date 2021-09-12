5668ba38c6f94ae24953fc292110d121c1aa47fa
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-09-12 11:11:23 +0200

    Day2Zad4

---
+++ src/Day2/Zad4.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class Zad4 {
+    public static void main(String[] args) {
+        zamienPrzecinkiKropki("Ala.ma.kota,a.kot,ma,Ale");
+    }
+    private static void zamienPrzecinkiKropki(String input) {
+        String result = input.replace(",","-STOP-").
+                replace(".","-STOP-");
+
+        System.out.println("Zmieniony tekst: "+ result);
+    }
+}
+
