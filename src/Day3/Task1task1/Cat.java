52feea48ef0218612ce20a9bf8da9f69f57f0f88
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-09-18 13:34:51 +0200

    Day3Task1task1

--- src/Day3/Task1task1/Cat.java
+++ src/Day3/Task1task1/Cat.java
@@ -1,4 +1,37 @@
 package Day3.Task1task1;
 
 public class Cat {
+
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void makeSound(){
+        System.out.println("Miau Miau");
+    }
+
+    public void eatMice(int mice){
+        System.out.println("I ate " + mice + "mice");
+    }
+    public void print(){
+        String description = "Cat: \nname = " + name;
+        System.out.println(description);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/Day3/Task1task1/Main.java
+++ src/Day3/Task1task1/Main.java
@@ -1,4 +1,29 @@
 package Day3.Task1task1;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Mruczek");
+        cat1.print();
+        cat1.makeSound();
+        cat1.eatMice(6);
+        System.out.println();
+
+        Cat cat2 = new Cat("Filemon");
+        cat1.print();
+        cat1.makeSound();
+        cat1.eatMice(12);
+        System.out.println();
+
+        Cat cat3 = new Cat("Bonifacy");
+        cat1.print();
+        cat1.makeSound();
+        cat1.eatMice(20);
+
+        cat1.setName("Rudy");
+        System.out.println("Nowe imie dla cat1: " + cat1.getName());
+
+     }
+
 }
